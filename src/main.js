7080d9014e6bfa52f74ba0ed1eea6028de16b3e7
Ethien Salinas <ethien.salinas@gmail.com>
2017-05-16 07:04:34 -0500

    ADD MovieFilter and CheckFilter single file components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres';
 import MovieList from './components/MovieList.vue'
+import MovieFilter from './components/MovieFilter.vue'
 
 new Vue({
     el: '#app',
@@ -23,43 +23,6 @@ new Vue({
     },
     components: {
         MovieList,
-        'movie-filter': {
-            data: function(){
-                return{
-                    genres
-                }
-            },
-            template:  `<div id='movie-filter'>
-                    <h2>Filter results</h2>
-                    <div class='filter-group'>
-                        <check-filter v-for='genre in genres' v-bind:title='genre' v-on:check-filter='checkFilter'></check-filter>
-                    </div>
-                </div>`,
-            methods: {
-                checkFilter(category, title, checked){
-                    this.$emit('check-filter', category, title, checked);
-                }
-            },
-            components: {
-                'check-filter': {
-                    data() {
-                        return {
-                            checked: false
-                        }
-                    },
-                    props: ['title'],
-                    template: `<div v-bind:class="{'check-filter':true, active:checked}" v-on:click='checkFilter'>
-                        <span class='checkbox'></span>
-                        <span class='check-filter-title'>{{title}}</span>
-                    </div>`,
-                    methods: {
-                        checkFilter(){
-                            this.checked = !this.checked;
-                            this.$emit('check-filter', 'genre', this.title, this.checked);
-                        }
-                    }
-                }
-            }
-        }
+        MovieFilter
     }
 });
