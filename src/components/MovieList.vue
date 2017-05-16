334d8b90cc431e39db46eb08cfbce788792290b1
Ethien Salinas <ethien.salinas@gmail.com>
2017-05-16 06:50:49 -0500

    ADD MovieList single file component

---
+++ src/components/MovieList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id='movie-list'>
+        <div v-for='movie in filteredMovies' class='movie'>{{movie.title}}</div>
+    </div>
+</template>
+
+<script>
+    import genres from '../util/genres';
+    export default {
+        data: function(){
+            return{
+                movies: [
+                    {title: 'Pulp Fiction', genre: genres.SCI_FI},
+                    {title: 'None Alone', genre: genres.FAMILY},
+                    {title: 'Austin Powers', genre: genres.ACTION}
+                ]
+            }
+        },
+        props: ['genre', 'time'],
+        methods: {
+            moviePassesGenreFilter(movie){
+                if(!this.genre.length){
+                    return true;
+                }else{
+                    return this.genre.find(genre => movie.genre === genre);
+                }
+            }
+        },
+        computed: {
+            filteredMovies(){
+                return this.movies.filter(this.moviePassesGenreFilter);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import './style.scss';
 import genres from './util/genres';
+import MovieList from './components/MovieList.vue'
 
 new Vue({
     el: '#app',
@@ -21,35 +22,7 @@ new Vue({
         }
     },
     components: {
-        'movie-list' : {
-            template: `<div id='movie-list'>
-                    <div v-for='movie in filteredMovies' class='movie'>{{movie.title}}</div>
-                </div>`,
-            data: function(){
-                return{
-                    movies: [
-                        {title: 'Pulp Fiction', genre: genres.SCI_FI},
-                        {title: 'None Alone', genre: genres.FAMILY},
-                        {title: 'Austin Powers', genre: genres.ACTION}
-                    ]
-                }
-            },
-            props: ['genre', 'time'],
-            methods: {
-                moviePassesGenreFilter(movie){
-                    if(!this.genre.length){
-                        return true;
-                    }else{
-                        return this.genre.find(genre => movie.genre === genre);
-                    }
-                }
-            },
-            computed: {
-                filteredMovies(){
-                    return this.movies.filter(this.moviePassesGenreFilter);
-                }
-            }
-        },
+        MovieList,
         'movie-filter': {
             data: function(){
                 return{
