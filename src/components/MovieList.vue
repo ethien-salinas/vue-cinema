7d363ef109c6060fc44c9cd0b7fc5a98f4724a65
Ethien Salinas <ethien.salinas@gmail.com>
2017-05-21 01:19:59 -0500

    ADD movie list using api call

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,22 +1,13 @@
 <template>
     <div id='movie-list'>
-        <div v-for='movie in filteredMovies' class='movie'>{{movie.title}}</div>
+        <div v-for='movie in filteredMovies' class='movie'>{{movie.movie.Title}}</div>
     </div>
 </template>
 
 <script>
     import genres from '../util/genres';
     export default {
-        data: function(){
-            return{
-                movies: [
-                    {title: 'Pulp Fiction', genre: genres.SCI_FI},
-                    {title: 'None Alone', genre: genres.FAMILY},
-                    {title: 'Austin Powers', genre: genres.ACTION}
-                ]
-            }
-        },
-        props: ['genre', 'time'],
+        props: ['genre', 'time', 'movies'],
         methods: {
             moviePassesGenreFilter(movie){
                 if(!this.genre.length){
