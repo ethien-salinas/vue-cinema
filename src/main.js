604946433bec6399d48ff966d485cbbb13800ca1
Ethien Salinas <ethien.salinas@gmail.com>
2017-05-23 07:51:51 -0500

    ADD formatted session-time

--- src/main.js
+++ src/main.js
@@ -3,15 +3,20 @@ import './style.scss';
 import MovieList from './components/MovieList.vue'
 import MovieFilter from './components/MovieFilter.vue'
 import VueResource from 'vue-resource';
+import moment from 'moment-timezone';
 
 Vue.use(VueResource);
+
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
 new Vue({
     el: '#app',
     data: {
         genre: [],
         time: [],
-        movies: []
+        movies: [],
+        moment
     },
     methods:{
         checkFilter(category, title, checked){
